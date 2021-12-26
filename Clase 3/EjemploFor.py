32695c3320e125025e5d0680be89aaef3cb275a8
christian <christian.yaranga@pucp.pe>
2021-12-26 15:23:38 -0500

    ADD: Ejemplo for

--- Clase 3/EjemploFor.py
+++ Clase 3/EjemploFor.py
@@ -1,8 +1,10 @@
 mascotas = ['perro', 'gato', 'perro', 'goldfish', 'gato', 'conejo', 'gato']
 print(mascotas)
 
-while mascotas:
-    print(mascotas.pop(-1))
+# while mascotas:
+#     print(mascotas.pop(-1))
         
+for mascota in mascotas:
+    print(mascota)
 
 print(mascotas)
