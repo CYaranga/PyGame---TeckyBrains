63536bc988bf24f311e6aef497b98f56da7aea21
christian <christian.yaranga@pucp.pe>
2021-12-26 16:56:56 -0500

    ADD: event close

--- Clase 5/main.py
+++ Clase 5/main.py
@@ -8,4 +8,10 @@ screen = pygame.display.set_mode((288,512))
 
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("Muchas gracias")
+            pygame.quit()
+            sys.exit()
+            
     pygame.display.update()
