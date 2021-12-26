8c0073a32a1d3816ac10e6abc4f1e59738319200
christian <christian.yaranga@pucp.pe>
2021-12-26 15:44:16 -0500

    ADD: Presentar

--- Clase 4/IntroduccionClase.py
+++ Clase 4/IntroduccionClase.py
@@ -6,8 +6,14 @@ class Humano():
         self.nombre = nombre 
         self.ocupacion = ocupacion
 
+    def presentar(self):
+        presentacion = ("Hola soy {}, mi edad es {} y mi ocupación es {}") 
+        print(presentacion.format(self.nombre, self.edad, self.ocupacion)) 
+
 Persona1 = Humano(31, "Pedro", "Ingeniero") 
 
 print(Persona1.edad)
 print(Persona1.nombre)
 print(Persona1.ocupacion)
+
+Persona1.presentar()
