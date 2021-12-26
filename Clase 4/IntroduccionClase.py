22dfcccac958b43602307ab9570a3263e693a54f
christian <christian.yaranga@pucp.pe>
2021-12-26 15:46:07 -0500

    ADD: Metodo con variable de insert

--- Clase 4/IntroduccionClase.py
+++ Clase 4/IntroduccionClase.py
@@ -10,10 +10,18 @@ class Humano():
         presentacion = ("Hola soy {}, mi edad es {} y mi ocupación es {}") 
         print(presentacion.format(self.nombre, self.edad, self.ocupacion)) 
 
+    def contratar(self, puesto): #añadimos un nuevo parámetro en el método
+        self.puesto = puesto
+        print ("{} ha sido contratado como {}".format(self.nombre, self.puesto))
+        self.ocupacion = puesto #Ahora cambiamos el atributo ocupación
+
 Persona1 = Humano(31, "Pedro", "Ingeniero") 
 
 print(Persona1.edad)
 print(Persona1.nombre)
 print(Persona1.ocupacion)
 
+Persona1.presentar()
+
+Persona1.contratar("Programador")
 Persona1.presentar()
