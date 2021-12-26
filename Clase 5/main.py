9e251c6dd9cd30c6457a74313620a7e7f7a4f3b0
christian <christian.yaranga@pucp.pe>
2021-12-26 18:11:45 -0500

    ADD: FPS

--- Clase 5/main.py
+++ Clase 5/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 pygame.init()
 
 screen = pygame.display.set_mode((288,512))
-
+fps = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
@@ -13,5 +13,6 @@ while True:
             print("Muchas gracias")
             pygame.quit()
             sys.exit()
-            
+
     pygame.display.update()
+    fps.tick(60)
