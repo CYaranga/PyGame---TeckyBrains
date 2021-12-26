a0c047380b6a4bc4a49c339b111b355f760862f0
christian <christian.yaranga@pucp.pe>
2021-12-26 13:48:42 -0500

    ADD: Ejemplo con multiples casos

--- Clase 2/IntroduccionCondicionales.py
+++ Clase 2/IntroduccionCondicionales.py
@@ -1,7 +1,15 @@
-variable1 = 5
-variable2 = 10
+lloviendo = input("Esta lloviendo? Si o No?").lower()
 
-if variable1 > variable2:
-  print("Variable1 es mayor!")
+if lloviendo == "si":
+  ##ocurre la primera linea de tiempo
+  print("Abrigate!!!!")
+  
+elif lloviendo == "no se":
+  #ocurre la segunda linea de tiempo
+  print("ve a fijarte")
+elif lloviendo == "tal vez":
+  print("Hoy no sales!")
 else:
-  print("Variable2 es mayor!")
+  print("Puedes salir al parque")
+# if lloviendo == "No":
+#   print("Puedes salir al parque")
