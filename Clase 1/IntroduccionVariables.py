745c137e43035a3dfb88e5ed76ec86b7b236e924
christian <christian.yaranga@pucp.pe>
2021-12-26 12:35:58 -0500

    ADD: Ejemplos de variables

--- Clase 1/IntroduccionVariables.py
+++ Clase 1/IntroduccionVariables.py
@@ -1,2 +1,16 @@
 #Ejemplo de comentarios
 print("Hola mundo")
+
+
+#tipo entero conocidas como: Integer o int
+variable = 25
+variable2 = 50
+
+
+#tipo decimal: float
+variable3 = 3.14 #numero pi
+
+
+#tipo texto: string
+variable4 = "Hola, que tal?"
+print(variable4)
