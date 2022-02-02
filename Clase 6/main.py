a02c369f439f788687812377a3e3d7ac804a0beb
christian <christian.yaranga@pucp.pe>
2022-02-02 07:12:49 -0500

    EDT: Clean comments

--- Clase 6/main.py
+++ Clase 6/main.py
@@ -9,24 +9,11 @@ fps = pygame.time.Clock()
 imagenDeFondo = pygame.image.load(Path(__file__).absolute().parent / "assets/background-day.png")
 
 #Personaje:
-#Gravedad del Personaje:
-#Es bastante sensible modificar de a pocos y usar como punto de dificultad del juego
 gravedad = 0.1
-
-#la "velocidad" a la que esta cayendo el personaje en un instante
 velocidadPersonajeY = 0
-#Posiciones del personaje
 posPersonajeY = 256
 posPersonajeX = 70
-
-#Imagen del Personaje
 imagenPersonaje = pygame.image.load(Path(__file__).absolute().parent / "assets/redbird-midflap.png")
-
-#Abstrae el rect de un personaje teniendo como punto de referencia su centro, que estará ubicado en la posicion (70,256).
-# Esto se puede leer como que el rect es un marco para una pintura. La pintura(imagenPersonaje) necesita estar en un cuadro. 
-# Y este cuadro tiene propiedades, como una pocision que depende de su punto de referencia, en este caso centro. 
-# Puede ser bottomCenter(el centro del lado Inferior) como un ejemplo. 
-# el principal uso de este es el poder detectar colisiones con otros rect
 personajeRect = imagenPersonaje.get_rect(center = (70,256))
 
 #Terreno:
@@ -42,19 +29,14 @@ while True:
             pygame.quit()
             sys.exit()
 
-        #Detectamos el evento de una tecla presionada
         if event.type == pygame.KEYDOWN:
-            #Se detecta si esta tecla pertenece a la tecla espacio
             if event.key == pygame.K_SPACE:
-                #Se para en seco el personaje y se le aplica una velocidad contraria
                 velocidadPersonajeY = -3
 
     screen.blit(imagenDeFondo, (0,0))
 
     #Dibujar personaje:
-    #Se agrega la gravedad a la posicion como si fuera la aceleración
     velocidadPersonajeY += gravedad
-    #La velocidad aplica a la pocision del personaje SOLO en Y
     personajeRect.centery += velocidadPersonajeY
     screen.blit(imagenPersonaje, personajeRect)
 
